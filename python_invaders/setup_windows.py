8fb19250ddb71cfe7db425a3aa19127bcc779421
Zachary Farley <Zachary Farley>
2017-01-21 19:11:33 -0800

    changes made to windows setup.py to include syntax fixes

--- python_invaders/setup_windows.py
+++ python_invaders/setup_windows.py
@@ -1,9 +1,9 @@
-application_title="Space Invaders" #name of application
+application_title= "Space Invaders" #name of application
 main_python_file="space_invaders.py"#Name of python application
 icon="spaceship_icon.png"#icon of application
 include_files=["ammo.png", "ship.png", "ufo.png"]#files needed for applicatino
 your_name= "Zachary Farley"
-program_description="The retro space invaders game"
+program_description= "The retro space invaders game"
 
 import sys
 
@@ -14,10 +14,10 @@ if sys.platform=="win32":
     base="Win32GUI"
 
 setup (
-            name= application_title
-            version= "1.0"
-            description=program_description
-            author=your_name
-            options-{"build_exe":{"icon":icon, "include_files":include_files}},
-                executables=[Executable(main_python_file,base-base)]
+            name= application_title,
+            version= "1.0",
+            description=program_description,
+            author=your_name,
+            options={"build_exe":{"icon":icon, "include_files":include_files}},
+                executables=[Executable(main_python_file,base=base)]
 )

498a01cdbdf4550d923078248de39e865c979d5a
Zach Farley <ZFarley@sisfirst.com>
2017-09-17 23:50:03 -0400

    mbile app test creation using kivy in python

---
+++ cs50x/cs50_week3.py
@@ -0,0 +1,14 @@
+
+#This program was made following the examples shown within the youtube video of harvard cs50x
+
+
+array = []
+print ("What is your name human?")
+name= raw_input()
+
+array.append(name)
+print array  
+
+    
+    
+

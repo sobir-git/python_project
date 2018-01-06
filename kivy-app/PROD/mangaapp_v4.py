a41cb9ea0090ecc10496ce1e8abd85f53b6a1917
Zach Farley <ZFarley@sisfirst.com>
2018-01-06 18:03:01 -0500

    python file for updated script

--- kivy-app/PROD/mangaapp_v4.py
+++ kivy-app/PROD/mangaapp_v4.py
@@ -1,6 +1,6 @@
-"""This is version 4 TEST of the manga app ment to change the image load option from using a
-web link to a local link. This will be testing the load file feature of kivy in 
-attempt to load the picture using the image located in an image directory"""
+"""This is version 4 PROD of the manga app this version cleans up the excess codes and functions with no
+noticeable bugs. If any bugs or improvements are offered please feel free to upload it to the github
+page belonging to zeke13210. Enjoy!! """
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
@@ -21,9 +21,10 @@ def create_connection(db_file):
     specified by the db_file
     :param db_file: database file
     :return: Connection object or None
+    creates image directory if one doesn't exist already
     """
     try:
-        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))#generates root path from the system  file runs on
         db_path = os.path.join(BASE_DIR, "manga.db")
         conn = sqlite3.connect(db_path)
         cur = conn.cursor()  # create sqlite cursor object allowing sql statements to be executed
@@ -35,8 +36,8 @@ def create_connection(db_file):
     except Error as e:
         with conn:
             cur.execute("CREATE TABLE manga_list (id    INTEGER PRIMARY KEY,name  STRING,url   STRING,image STRING);")
-        print("created previously missing table, please run application again")
         return None
+#The below code is the kivy builder used to create the GUI for the application
 Builder.load_string("""
 #kivy `1.10.0`
 <RootWidget>:
@@ -106,23 +107,17 @@ class RootWidget(BoxLayout):
     url_input = ObjectProperty()
     pic_input = ObjectProperty()
     delete_list= []#array used to create a delete list to remove multiple rows when pressing delete button. #NOTE- currently not functioning
-    #database= "pythonDB_TEST.db"#database containing app data
-    img_delete_list=[]
-    os.environ['KIVY_IMAGE']= 'pil,sdl2'
-    database= "manga.db"
+    os.environ['KIVY_IMAGE']= 'pil,sdl2'#used to set window loader for kivy
+    database= "manga.db"#DB file where information is stored. Needs to stay in same directory space of file
  
         
     def select_all_tasks(self, conn):
         """This method performs a select all SQL statment and populates the BoxLayout
         With the Button, Label and AsyncPicture widgets."""
         cur = conn.cursor()#create sqlite cursor object allowing sql statements to be executed
-        
         cur.execute("SELECT * FROM manga_list")
         db_list = cur.fetchall()#creates list of manga_list table allowing for loop to run and populate the BoxLayout 
-        """if statement added below is a test function that is going to be used all lbl, pic and btn
-        widgets before repopulating them again. The function looks for the boolean var function_run to see
-        if the function delete_row() has ran. *NOTE: Functionality still under works"""
-        
+
         for row in db_list:
             """Label widget generated with font 30px and markup property. The markup allows the
             func. populate_delete_row to run when label is pressed"""
@@ -138,10 +133,7 @@ class RootWidget(BoxLayout):
 
     def __init__(self, **kw):
         super(RootWidget, self).__init__(**kw)
-        #database = "C:\\sqlite\db\pythonsqlite.db"
-        #database= "C:\\Users\zfarley\Documents\Development\python_project\kivy-app\data\pythonDB.db"#location 1 DB connection
-
-        # create a database connection
+        # create a database connection to the global var database
         conn = create_connection(self.database)
         with conn:
             self.select_all_tasks(conn)
@@ -162,26 +154,13 @@ class RootWidget(BoxLayout):
         self.ids.grid.clear_widgets()#removes widgets from scroll view id of grid
         with conn:
             self.select_all_tasks(conn)
-        """cur.execute("SELECT * FROM manga_list ORDER BY id DESC limit 1")
-        new_line= cur.fetchall()#using previous SELECT statment this turns the data into an iterable list
-        for item in new_line:
-            lbl= Label(text='[ref='']' + item[1]+'[/ref]', id=item[1], font_size=30, markup=True, on_ref_press=self.populate_delete_row)
-            pic= AsyncImage(source=item[3])#picture widgetmade with image address given by user located in col 3
-            btn= Button(text='OPEN WEBPAGE', size_hint_y=None, id=item[2])#button widget 
-            btn.bind(on_release=self.open_url)#gives btn widget action to run open_url method when pressed
-                
-            #The below is used to place the newly declared widgets onto the BoxLayout
-            self.ids.grid.add_widget(lbl)
-            self.ids.grid.add_widget(pic)
-            self.ids.grid.add_widget(btn)"""
-            
         #The below declaration sets the text value for all inuts to empty
         self.ids.name.text= ''
         self.ids.url.text=''
         self.ids.pic.text=''
 
     def open_url(self, weblink):
-        """This function opens a url given by the user"""
+        """This function opens a url given by the user when clicking button"""
         webbrowser.open(weblink.id)
 
     def populate_delete_row(self, label_name, third):
@@ -196,18 +175,14 @@ class RootWidget(BoxLayout):
             print('else statement in populate delete row')
             label_name.text= '[b][color=#5253e2][ref=]'+label_name.id+'[/ref][/color][/b]'#change color of text
             label_name.texture_update()#updates color change 
-            self.delete_list.append(label_name.id)#append selected item to delete_list
+            self.delete_list.append(label_name.id)#append selected item to delete_list var
        
     def remove_delete_object(self, label_name): 
         """This function removes a label_name.id from the deleted list global variable. This function
-        also changes the color back the original white color when the label is first populated
-        *NOTE- currently under development only works partially""" 
-        print ('remove delete object ran')
+        also changes the color back the original white color when the label is first populated""" 
         label_name.text='[color=#ffffff][ref=]'+label_name.id+'[/ref][/color]' #change color of text back to white color
         label_name.texture_update()#updates label widget texture
         self.delete_list.remove(label_name.id)#removes label_name.id from delete list
-        print(self.delete_list)
-        self.function_run= True
 
     def delete_row(self, **kwargs):
         """This function uses the delete list array to execute a delete statement according to the label ID of the 
@@ -219,26 +194,25 @@ class RootWidget(BoxLayout):
         conn = create_connection(self.database)
         cur = conn.cursor()
         for row in array:
-            cur.execute("SELECT image FROM manga_list WHERE name in (?)", (row,))
-            img_list= cur.fetchall()
-            img_string= str(img_list).strip('([,\'])')
-            os.remove('images/' + img_string)
-            print(img_string)
-            cur.execute("DELETE FROM manga_list WHERE name in (?)", (row,))
+            cur.execute("SELECT image FROM manga_list WHERE name in (?)", (row,))#pulls image name from label name in same row
+            img_list= cur.fetchall()#converts kivy obj into list object
+            img_string= str(img_list).strip('([,\'])')#converts list object in string object and removes excess chars
+            os.remove('images/' + img_string)#removes associated image from images/ when label is removed
+            cur.execute("DELETE FROM manga_list WHERE name in (?)", (row,))#removes row from sqlite table 
             
         self.ids.grid.clear_widgets()#removes widgets from scroll view id of grid    
         with conn:
             self.select_all_tasks(conn)
         self.delete_list=[]#empty delete list for future use
+
     def instructions(self, **kwargs):
+        """This function is a popup function that displays functions for the users to show them how to use the application"""
         popup = Popup(title='Instructions',
         content=Label(
             text='WELCOME TO THE MANGA APP!!! \nTo use this app you need 3 things\n1. title \n2.a web URL\n3.Image name (*include extension .png, .img...etc)\n(*see note below) \nOnce you save a new line will appear in the menu.\n*NOTE: You must manually save an image\nto the images directory\nfor the pic to appear in the app \nENJOY!!'),
         size_hint=(None, None), size=(400, 300))
         popup.open()
         
-        
-
 class MainApp(App):
     def build(self):
         return RootWidget()
